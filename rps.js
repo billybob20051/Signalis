eef5f9182d811f005902d0ce84bc3c1b2da36c36
billybob20051 <steven.j.harris@outlook.com>
2023-05-01 17:27:56 -0500

    Added JS file, text changes upon clicking it

---
+++ rps.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Or Die Trying?";
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.fontSize = "70px";
+  }
